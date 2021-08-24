027ec6691a5b22920b01bcf4a1f4f6861b6aaec2
RoNSipinoa <whsoh@naver.com>
2021-08-24 12:41:11 +0900

    organize codes
    brailleConvert.py and brailleToolkit.py into braille.py
    codeCreate.py renamed into infoCodeCreate.py

--- code/braille.py
+++ code/braille.py
@@ -252,3 +252,36 @@ def braille_convert(string):
             output_string += k  # 최종 산출이 들어가는 answerstring에 각 문자의 braille에 들어간 리스트에 있는 문자를 하나씩 결합
 
     return output_string        # 출력
+
+def braille_decompose(string):
+    output_list = []
+    for i in list(" ".join(string.split()[2:])):
+        a = ord(i) - 10240
+        braille_string = ''
+        if a == 0:
+            braille_string = '0'
+        for j in range(8, 0, -1):
+            if a - 2 ** (j - 1) >= 0:
+                braille_string = str(j) + braille_string
+                a -= 2 ** (j - 1)
+            else:
+                pass
+        output_list.append(braille_string)
+    return " ".join(output_list)
+
+
+def braille_compose(string):
+    a = string.split()[2:]
+    output_string = ''
+    for i in a:
+        n = 0
+        i = list(set(i))
+        for j in i:
+            if int(j) not in [0, 1, 2, 3, 4, 5, 6, 7, 8]:
+                raise ValueError
+            if int(j) == 0:
+                n = 0
+            else:
+                n += 2 ** (int(j) - 1)
+        output_string += chr(10240 + n)
+    return output_string
--- code/main.py
+++ code/main.py
@@ -3,37 +3,26 @@ import discord
 import hgtk
 import time
 import os
-import brailleConvert
+import braille
 import secret
 import bot_help
 import morsecodeConvert
-import codeCreate
+import infoCodeCreate
 import md5Battle
-import choDown
-import brailleToolkit
 import mineSweepers
 
 client = discord.Client()
 
-def bot_log(message, command_name):
+def log(message, command_name):
     if len(message.content.split()) == 1 and not message.content.startswith('\\!'):
         string = ''
-        outfile = open('botlog.txt', 'a')
         print(f'{hgtk.josa.attach(message.author.name, hgtk.josa.I_GA)} {command_name} 명령어 사용')
-        outfile.write(f'\n{message.author.name} {command_name}')
-        outfile.close()
     elif len(message.content.split()) > 1:
         string = " ".join(message.content.split()[1:])
-        outfile = open('botlog.txt', 'a')
         print(f'{hgtk.josa.attach(message.author.name, hgtk.josa.I_GA)} {command_name} 명령어 사용하여 {string} 실행')
-        outfile.write(f'\n{message.author.name} {command_name} {string}')
-        outfile.close()
     else:
         string = message.content[2:]
-        outfile = open('botlog.txt', 'a')
         print(f'{hgtk.josa.attach(message.author.name, hgtk.josa.I_GA)} {command_name} 명령어 사용하여 {string} 실행')
-        outfile.write(f'\n{message.author.name} {command_name} {string}')
-        outfile.close()
 
 
 @client.event
@@ -44,10 +33,6 @@ async def on_ready():
     print(f'version: {discord.__version__}')
     print("==========")
     await client.change_presence(status=discord.Status.online, activity=discord.Game(name="!도움"))
-    outfile = open('botlog.txt', 'a')
-    outfile.write('\n\n')
-    outfile.write(time.ctime())
-    outfile.close()
 
 
 @client.event
@@ -67,42 +52,42 @@ async def on_message(message):
         if message.content.startswith('\\!'):
             await message.delete()
             await message.channel.send(message.content[2:])
-            bot_log(message, '숨은출력')
+            log(message, '숨은출력')
 
         if message.content.startswith('!'):
             if content.split()[0] == '!출력':
                 await message.channel.send(message.content[4:])
-                bot_log(message, '출력')
+                log(message, '출력')
 
             if message.content == '!안녕':
                 await message.channel.send('만식이여유~! 진지 잡쉈슈?')
-                bot_log(message, '안녕')
+                log(message, '안녕')
 
             if message.content == '!핑':
                 await message.channel.send(f'{round(client.latency * 100)}ms!')
-                bot_log(message, '핑')
+                log(message, '핑')
 
             if content.split()[0] == '!도움':
                 if len(message.content.split()) == 1:
                     await message.channel.send(embed=bot_help.explainWholeCommands())
                 else:
                     await message.channel.send(embed=bot_help.explainCommands(message.content.split()[1]))
-                bot_log(message, '도움')
+                log(message, '도움')
 
             if message.content == '!채널':
                 embed = discord.Embed(title='채널 목록!', description=message.guild.name, color=discord.Color.from_rgb(184341))
                 for var in message.guild.text_channels:
                     embed.add_field(name='', value=var.name, inline=False)
                 await message.channel.send(embed=embed)
-                bot_log(message, '채널')
+                log(message, '채널')
 
             if message.content == '!프사':
                 await message.channel.send(message.author.avatar_url)
-                bot_log(message, '프사')
+                log(message, '프사')
 
             if content.split()[0] == '!유닉스시간':
                 await message.channel.send(time.time())
-                bot_log(message, '유닉스시간')
+                log(message, '유닉스시간')
 
             if content.split()[0] == '!달력':
                 cmd = message.content.split()
@@ -117,55 +102,51 @@ async def on_message(message):
                     await message.channel.send('```' + cal + '```')
                 else:
                     await message.channel.send('연도 또는 연도+월을 입력해주셔유!')
-                bot_log(message, '달력')
+                log(message, '달력')
 
             if content.split()[0] == '!시간':
                 await message.channel.send(f'지금 시각은 {time.ctime()}래유~')
-                bot_log(message, '시간')
+                log(message, '시간')
 
             if content.split()[0] == '!qr' or content.split()[0] == '!QR':
-                return_value = codeCreate.qrcode_create(message.content)
+                return_value = infoCodeCreate.qrcode_create(message.content)
                 if len(return_value) == 1:
                     await message.channel.send(return_value)
                 else:
                     await message.channel.send(file=return_value[0])
                     os.remove(return_value[1])
-                bot_log(message, 'qr')
+                log(message, 'qr')
 
             if content.split()[0] == '!바코드':
-                return_value = codeCreate.barcode_create(message.content)
+                return_value = infoCodeCreate.barcode_create(message.content)
                 if len(return_value) == 1:
                     await message.channel.send(return_value)
                 else:
                     await message.channel.send(file=return_value[0])
                     os.remove(return_value[1])
-                bot_log(message, 'qr')
+                log(message, 'qr')
 
             if content.split()[0] == '!모스부호':
                 await message.channel.send(morsecodeConvert.morsecode_convert(message.content))
-                bot_log(message, '모스부호')
+                log(message, '모스부호')
 
             if content.split()[0] == '!점자':
-                await message.channel.send(brailleConvert.braille_convert(message.content))
-                bot_log(message, '점자')
+                await message.channel.send(braille.braille_convert(message.content))
+                log(message, '점자')
 
             if content.split()[0] == '!점자변환':
                 if message.content.split()[1] == '-숫자로':
-                    await message.channel.send(brailleToolkit.braille_decompose(message.content))
-                    bot_log(message, '점자변환')
+                    await message.channel.send(braille.braille_decompose(message.content))
+                    log(message, '점자변환')
                 elif message.content.split()[1] == '-점자로':
-                    await message.channel.send(brailleToolkit.braille_compose(message.content))
+                    await message.channel.send(braille.braille_compose(message.content))
                 else:
                     await message.channel.send('잘못된 옵션여유!')
-                bot_log(message, '점자변환')
-
-            if content.split()[0] == '!촛엉':
-                await message.channel.send(choDown.cho_down(message))
-                bot_log(message, '촛엉')
+                log(message, '점자변환')
 
             if content.split()[0] == '!md5' or content.split()[0] == '!md5배틀':
                 await md5Battle.md5_battle(message)
-                bot_log(message, 'md5배틀')
+                log(message, 'md5배틀')
 
             if content.split()[0] == '!지뢰찾기':
                 a = message.content.split()
